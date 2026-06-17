88fe94d659b97f18ab5aa6a3b87c483a1626ac74
akio1230 <huangxinqiuwei@gmail.com>
2026-06-17 18:53:32 +0900

    1

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,23 @@
+﻿// ConsoleApplication1.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include <iostream>
+
+class Character
+{
+private:
+    char name;
+    int hp;
+    int attack;
+
+public:
+    void Character
+};
+
+
+
+int main()
+{
+    
+}
+
