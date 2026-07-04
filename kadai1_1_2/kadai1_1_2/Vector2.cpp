a2549c69b53d04267f12282b21af8962c37b61dc
akio1230 <huangxinqiuwei@gmail.com>
2026-07-04 09:59:01 +0900

    1

--- kadai1_1_2/kadai1_1_2/Vector2.cpp
+++ kadai1_1_2/kadai1_1_2/Vector2.cpp
@@ -1,1 +1,9 @@
 #include "Vector2.h"
+
+Vector2 Vector2::operator +(const Vector2& vec2) const
+{
+	int x = x + vec2.x;
+	int y = y + vec2.y;
+
+	return Vector2(x, y);
+}
--- kadai1_1_2/kadai1_1_2/Vector2.h
+++ kadai1_1_2/kadai1_1_2/Vector2.h
@@ -2,7 +2,19 @@
 class Vector2
 {
 public:
+	Vector2() = default;
+	Vector2(int x, int y) : x(x), y(y){}
 
+	~Vector2() = default;
+
+public:
+	int operator +(const Vector2& vec2) const;
+
+	int operator -(const Vector2& vec2) const;
+
+	int operator ==(const Vector2& vec2) const;
+
+	int operator <<(const Vector2& vec2) const;
 
 public:
 	int x = 0;
